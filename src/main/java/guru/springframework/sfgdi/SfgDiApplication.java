7bbf45e0ed27afff273cd557c96bedf20cdd4a3c
azer-mirzayev <azer.mirzayev@digirella.az>
2023-02-09 14:35:43 +0400

    30-41 spring bean life cycle

--- src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
+++ src/main/java/guru/springframework/sfgdi/SfgDiApplication.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgdi;
 import guru.springframework.sfgdi.controllers.ConstructorInjectedController;
 import guru.springframework.sfgdi.controllers.I18nController;
 import guru.springframework.sfgdi.controllers.MyController;
+import guru.springframework.sfgdi.controllers.PetController;
 import guru.springframework.sfgdi.controllers.PropertyInjectedController;
 import guru.springframework.sfgdi.controllers.SetterInjectedController;
 import org.springframework.boot.SpringApplication;
@@ -15,6 +16,13 @@ public class SfgDiApplication {
     public static void main(String[] args) {
         ApplicationContext context = SpringApplication.run(SfgDiApplication.class, args);
         MyController c = (MyController) context.getBean("myController");
+
+        System.out.println("---------- Pet");
+        PetController p = (PetController) context.getBean("petController");
+        System.out.println(p.whichPetIsTheBest());
+
+
+        System.out.println("---------- I18N");
 
         I18nController i18 = (I18nController) context.getBean("i18nController");
         System.out.println(i18.sayHello());
